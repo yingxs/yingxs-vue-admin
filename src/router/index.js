3ea1e55ae9448706d439ca93ef4301ba12fb6bcb
王昊 <yingxs888@126.com>
2019-11-05 18:07:27 +0800

    feat: 添加新组件

--- src/router/index.js
+++ src/router/index.js
@@ -196,11 +196,24 @@ export const asyncRoutes = [
     ]
   },
 
+  {
+    path: '/score-entering',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/scoreEntering/index'),
+        name: 'ScoreEntering',
+        meta: { title: '测试测试1', icon: 'nested', noCache: true }
+      }
+    ]
+  },
+
   /** when your routing map is too long, you can split it into small modules **/
-  // componentsRouter,
-  // chartsRouter,
-  // nestedRouter,
-  // tableRouter,
+  componentsRouter,
+  chartsRouter,
+  nestedRouter,
+  tableRouter,
 
   {
     path: '/example',
---
+++ src/views/scoreEntering/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="app-container">
+    <el-row :gutter="20">
+      <el-col :span="6" :xs="24">
+        <user-card :user="user" />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import UserCard from '../profile/components/UserCard'
+import Activity from '../profile/components/Activity'
+import Timeline from '../profile/components/Timeline'
+import Account from '../profile/components/Account'
+
+export default {
+  name: 'Profile',
+  components: { UserCard, Activity, Timeline, Account },
+  data() {
+    return {
+      user: {},
+      // activeTab: 'activity'
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'name',
+      'avatar',
+      'roles'
+    ])
+  },
+  created() {
+    this.getUser()
+  },
+  methods: {
+    getUser() {
+      this.user = {
+        name: "yingxs",
+        role: this.roles.join(' | '),
+        email: 'admin@test.com',
+        avatar: this.avatar
+      }
+    }
+  }
+}
+
+</script>
+Expected indentation of 0 spaces but found 2
+<style>
+</style>
